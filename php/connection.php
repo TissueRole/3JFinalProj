737d3db9bcfc77d020bb5dff9c3acca6ea7935ab
TissueRole <mangjose246@gmail.com>
2024-11-23 10:16:21 +0800

    added service images

---
+++ php/connection.php
@@ -0,0 +1,10 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "3jfinalproject";
+
+    $conn = ($con->connect_error){
+        die("Connection Failed". $conn->connec_error)
+    }
+?>
